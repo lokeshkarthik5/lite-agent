cc10a9cdce7b65be45193bd664856d926ce7dd60
lokeshkarthik5 <lokeshkarthic1999@gmail.com>
2025-06-19 13:32:58 +0530

    agent

--- liteagent/agent.py
+++ liteagent/agent.py
@@ -5,5 +5,20 @@ class Agent:
         self.name = name
         self.func = func
         self.model = model
-        self.prompt_template = prompt_template or None
+        self.prompt_template = prompt_template 
         self.tools = tools or None
+
+    def run(self,input_data,state):
+        prompt = self.prompt_template.format(input_data,state)
+        response = self.model.generate(prompt)
+        return self.func(response,state)
+
+def agent(name,model,prompt_template=None,tools=None):
+    def decorator(func):
+        a = Agent(name,func,model,prompt_template,tools)
+        @wraps(func)
+        def wrapper(input_data,state):
+            return a.run(input_data,state)
+        wrapper.agent = a
+        return wrapper
+    return decorator

76777e201d59d97c5f45090da52b0877553e2a90
lokeshkarthik5 <lokeshkarthic1999@gmail.com>
2025-06-19 13:36:20 +0530

    state.py

---
+++ liteagent/state.py
@@ -0,0 +1,13 @@
+
+
+class AgentState:
+    def __init__(self,input_data=None):
+        self.input_data = input_data or {}
+
+    def get(self,key,default=None):
+        return self.data_get(key,default)
+
+    def set(self,key,value):
+        self.input_data[key] = value
+
+    

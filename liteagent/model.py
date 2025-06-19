d29cb98b337fb5e9c794ecc234cdd9bc66766f97
lokeshkarthik5 <lokeshkarthic1999@gmail.com>
2025-06-19 13:57:13 +0530

    openai && anthropic

--- liteagent/model.py
+++ liteagent/model.py
@@ -6,4 +6,19 @@ class LLMProvider:
         self.model_name = model_name
         self.api_key = api_key
 
+    def openai_generate_text(self,prompt):
+        response = openai.Completion.create(
+            model=self.model_name,
+            prompt=prompt,
+            max_tokens=1000
+        )
+        return response.choices[0].text
+    
+    def anthropic_generate_text(self,prompt):
+        response = anthropic.Anthropic(api_key=self.api_key).messages.create(
+            model=self.model_name,
+            messages=[{"role": "user", "content": prompt}],
+            max_tokens=1000
+        )
+        return response.choices[0].message.content
     
